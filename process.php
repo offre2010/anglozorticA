98e71c2c460a27e5c97bcb7917c1292714137739
offre2010 <62150639+offre2010@users.noreply.github.com>
2024-01-07 18:35:54 +0100

    Update process.php - Success4
    Update Success 4

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://edgeledger.net/success.html');
+    header('Location: https://anglozortic.com/main.anglozortic.com/contact/success.html');
   }
